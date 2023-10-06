bc9e3f7c7d00b0c2098450e338a5f6c382546870
Luke Park <143543147+lukep258@users.noreply.github.com>
2023-10-06 17:08:02 -0500

    7

--- app.js
+++ app.js
@@ -15,34 +15,39 @@ function getRandomIntInclusive(min, max) {
 var correctAns = 50;//getRandomIntInclusive(1, 100);
 var correct = false;
 var name = prompt('Enter your name, please:')
-
 var guess = prompt("Guess a number from 1-100")
 var attempts = [];
+var names = {};
 
 function game(){
     while(correct === false){
         if(correctAns === parseInt(guess)){
             attempts.push(guess);
-            alert(`That's correct, ${name}! The answer was ${correctAns}. You got it right in ${attempts.length}. These are your guesses: ${attempts.join()}.`);
-            correct = true;
-            var playAgain = prompt(`Do you want to play again? Y/N`)
-            var tryAgain = false;
-            while(tryAgain === false){
-                if(playAgain === `Y`){
-                    attempts = [];
-                    correctAns = 40;
-                    tryAgain = true;
-                    guess = prompt("Guess a number from 1-100");
-                    correct = false;
-                    game();
-            }else if(playAgain ===(`N`||`n`)){
-                alert(`Thank you for playing!`);
+            if(names.name && attempts.length<names.name){
+                alert(`That's correct, ${name}! The answer was ${correctAns}. You got it right in ${attempts.length}. These are your guesses: ${attempts.join()}. You did ${names.name-attemps.length} guesses better than your previous try`);
+            }
+            else{
+                alert(`That's correct, ${name}! The answer was ${correctAns}. You got it right in ${attempts.length}. These are your guesses: ${attempts.join()}. You did ${attempts.length-names.name} guesses worse than your previous try`);
                 correct = true;
-                tryAgain = true;
-            }else{
-                alert(`Sorry, I don't understand that.`);
-                playAgain = prompt(`Do you want to play again? Y/N`)            
-            }}
+                var playAgain = prompt(`Do you want to play again? Y/N`)
+                var tryAgain = false;
+                while(tryAgain === false){
+                    if(playAgain === `Y`){
+                        attempts = [];
+                        correctAns = 40;
+                        tryAgain = true;
+                        guess = prompt("Guess a number from 1-100");
+                        correct = false;
+                        name = prompt('Enter your name, please:')
+                        game();
+                }else if(playAgain ===(`N`||`n`)){
+                    alert(`Thank you for playing!`);
+                    correct = true;
+                    tryAgain = true;
+                }else{
+                    alert(`Sorry, I don't understand that.`);
+                    playAgain = prompt(`Do you want to play again? Y/N`)            
+                }}}
         }else if((parseInt(guess) > 100) || parseInt(guess)< 1){
             alert(`Your number is not between 1-100, guess again, ${name}.`);
             guess = prompt(`Guess a number from 1-100`);
@@ -59,7 +64,7 @@ function game(){
             guess = prompt(`Guess a number from 1-100`);
       }
     }
-
 }
 
 game();
+
